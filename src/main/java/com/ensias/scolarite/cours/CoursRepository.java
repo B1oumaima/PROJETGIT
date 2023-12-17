bc441c941da10993433b27796268cd58e31ad6f8
B1oumaima <oumaimabencherif.2001@gmail.com>
2023-12-17 21:08:23 +0100

    comment number two

--- src/main/java/com/ensias/scolarite/cours/CoursRepository.java
+++ src/main/java/com/ensias/scolarite/cours/CoursRepository.java
@@ -1,5 +1,7 @@
 package com.ensias.scolarite.cours;
 
+//changement oumaima pour test
+
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 public interface CoursRepository extends CrudRepository<Cours, Integer>{
