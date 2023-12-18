52e0769bb5f6abae6fc658bd3a05198b495b9bfd
yahouza <kenzahassanine@gmail.com>
2023-12-18 20:15:58 +0100

    kjnj

--- src/main/java/com/ensias/scolarite/cours/CoursRepository.java
+++ src/main/java/com/ensias/scolarite/cours/CoursRepository.java
@@ -1,10 +1,6 @@
 package com.ensias.scolarite.cours;
 
-<<<<<<< HEAD
-//changement de kenza pour le test 
-=======
 //changement oumaima pour test
->>>>>>> new-feature
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
